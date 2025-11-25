65267f4a0953475e52670df500cc866dc9e3b805
Vv3n <kittinanwutthi@gmail.com>
2025-11-25 17:46:57 +0700

    Add files via upload

---
+++ 3.2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int studentId;
+    float score;
+};
+
+int main(){
+    int N, i;
+
+    printf("Enter Number of Student: ");
+
+    if(scanf("%d", &N) != 1){
+        return 1;
+    }
+
+
+    struct Student student[N];
+
+
+    for(i = 0; i < N; i++){
+
+        if(scanf("%d %f %s", &student[i].studentId, &student[i].score, student[i].name) != 3){
+            return 1;
+        }
+    }
+
+    for(i = 0; i < N; i++){
+        printf("ID: %d, Name: %s, Score: %.2f\n", student[i].studentId, student[i].name, student[i].score);
+    }
+
+    return 0;
+}
